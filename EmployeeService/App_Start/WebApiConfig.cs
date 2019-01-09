b211c39b50e5bf3b224a72c5dd54607fe8ffd73c
solomon <sorael11@gmail.com>
2019-01-09 14:49:24 -0500

    3 project commit

---
+++ EmployeeService/App_Start/WebApiConfig.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebApiContrib.Formatting.Jsonp;
+
+namespace EmployeeService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            var jsonpFormatter = new JsonpMediaTypeFormatter(config.Formatters.JsonFormatter);
+            config.Formatters.Insert(0,jsonpFormatter);
+        }
+    }
+}
---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,116 @@
+﻿using EmployeeDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Employee> AllEmployees()
+        {
+            using(EmployeeDBEntities db = new EmployeeDBEntities())
+            {
+                return db.Employees.ToList();
+            }
+        }
+        [HttpGet]
+        public HttpResponseMessage EmployeeWithId(int id)
+        {
+            using (EmployeeDBEntities db = new EmployeeDBEntities())
+            {
+                var emp = db.Employees.FirstOrDefault(x => x.ID == id);
+                if(emp == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "the employee with id " + id + " does not exist");
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, emp);
+                }
+            }
+        }
+
+        [HttpPost]
+        public HttpResponseMessage AddEmployee([FromBody]Employee emp)
+        {
+            try
+            {
+                using (EmployeeDBEntities db = new EmployeeDBEntities())
+                {
+                    db.Employees.Add(emp);
+                    db.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, emp);
+                    message.Headers.Location = new Uri(Request.RequestUri + "/" + emp.ID.ToString());
+                    return message;
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        [HttpPut]
+        public HttpResponseMessage Edit([FromUri]Employee emp, int id)
+        {
+            try
+            {
+                using(EmployeeDBEntities db = new EmployeeDBEntities())
+                {
+                    var e = db.Employees.FirstOrDefault(x => x.ID == id);
+                    if(e == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "The employee with the ID " + " does not found to be edited.");
+                    }
+                    else
+                    {
+                        e.FirstName = emp.FirstName;
+                        e.LastName = emp.LastName;
+                        e.Gender = emp.Gender;
+                        e.Salary = emp.Salary;
+
+                        db.SaveChanges();
+
+                        return Request.CreateResponse(HttpStatusCode.OK, e);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        [HttpDelete]
+        public HttpResponseMessage Remove(int id)
+        {
+            try
+            {
+                using(EmployeeDBEntities db = new EmployeeDBEntities())
+                {
+                    var e = db.Employees.FirstOrDefault(x => x.ID == id);
+                    if(e == null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.NotFound, "The employee with the ID " + " does not found to be edited.");
+                    }
+                    else
+                    {
+                        db.Employees.Remove(e);
+                        db.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, "The employee with ID " + id + " is deleted.");
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }            
+
+        }
+    }
+}
